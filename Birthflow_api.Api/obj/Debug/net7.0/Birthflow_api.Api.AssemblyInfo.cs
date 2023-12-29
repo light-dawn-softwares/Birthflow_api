d3e0827e0ea46c33eee1ac4ac7422966974b44c0
Russell Eduardo Soza Arevalo <endosoza1@gmail.com>
2023-12-28 18:45:22 -0600

    feat: Add partograph repository and services (#6)

--- Birthflow_api.Api/obj/Debug/net7.0/Birthflow_api.Api.AssemblyInfo.cs
+++ Birthflow_api.Api/obj/Debug/net7.0/Birthflow_api.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Birthflow_api.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+062ea79a6f58ae25498b1dd931dab9a92157aed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Birthflow_api.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Birthflow_api.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Birthflow_api.Infrastructure/Repositories/PartographRepository.cs
@@ -0,0 +1,50 @@
+﻿using Birthflow_api.Domain.Interfaces;
+using Birthflow_api.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthflow_api.Infrastructure.Repositories
+{
+    internal class PartographRepository : IPartographRepository
+    {
+        private readonly BirthflowDbContext _context;
+
+        PartographRepository(BirthflowDbContext _context) => this._context = _context;
+  
+        public void create(Partograph partograph, WorkTime workTime)
+        {
+            try
+            {
+                _context.Add(partograph);
+                _context.SaveChanges();
+
+                _context.Add(workTime);
+                _context.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void delete(string PartographId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Partograph> findAll(Guid UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Partograph partograph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
