ba8c4b20695180aa87809bb82b93d4550eac7ec5
Russell Soza <endosoza1@gmail.com>
2023-12-28 18:42:09 -0600

    feat: Add partograph repository and services

---
+++ Birthflow_api.Domain/Interfaces/IPartographRepository.cs
@@ -0,0 +1,20 @@
+﻿using Birthflow_api.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthflow_api.Domain.Interfaces
+{
+    public interface IPartographRepository
+    {
+        public void create(Partograph partograph, WorkTime workTime);
+
+        public void update(Partograph partograph);
+
+        public void delete(string PartographId);
+
+        public IEnumerable<Partograph> findAll(Guid UserId);
+    }
+}
